505b2a8c851d94d2c10e3bac55ce693ac8aeb2a2
nvjib <nvjibiman@gmail.com>
2025-09-19 23:03:57 +0100

    feat(posts): implement GET /posts/:id endpoint to get a single post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -36,7 +36,22 @@ const fetchAllPosts = async (req, res) => {
     return res.status(200).json(data)
 }
 
+const singlePost = async (req, res) => {
+    const { id } = req.params
+
+    const { data, error } = await supabase
+        .from("posts")
+        .select()
+        .eq("id", id)
+
+    if (error) return res.status(500).json({ error: error.message })
+    if (!data || data.length === 0) return res.status(404).json({ error: "Post not found" })
+
+    return res.status(200).json(data)
+}
+
 module.exports = {
     createPost,
-    fetchAllPosts
+    fetchAllPosts,
+    singlePost
 }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const router = express.Router()
 const { signUp, login } = require("../controllers/auth.controller")
-const { createPost, fetchAllPosts } = require("../controllers/post.controller")
+const { createPost, fetchAllPosts, singlePost } = require("../controllers/post.controller")
 
 router.post("/sign-up", signUp)
 router.post("/login", login)
 
 router.post("/posts", createPost)
 router.get("/posts", fetchAllPosts)
+router.get("/posts/:id", singlePost)
 
 module.exports = router
