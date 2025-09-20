ea92cbc57caecd9fb8dfd7b3623efbcb8878b0d8
nvjib <nvjibiman@gmail.com>
2025-09-21 00:03:50 +0100

    feat(posts): add authentication and authorization for readers and authors

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,11 +1,15 @@
 const express = require("express")
 const postsRouter = express.Router()
 const { createPost, fetchAllPosts, singlePost, updatePost, deletePost } = require("../controllers/post.controller")
+const { requireRole, authenticate } = require("../middleware/auth.middleware")
 
-postsRouter.post("/", createPost)
-postsRouter.get("/", fetchAllPosts)
-postsRouter.get("/:id", singlePost)
-postsRouter.put("/:id", updatePost)
-postsRouter.delete("/:id", deletePost)
+// Public routes (anyone can access)
+postsRouter.get("/", authenticate, fetchAllPosts)
+postsRouter.get("/:id", authenticate, singlePost)
+
+// Author-only routes (protected)
+postsRouter.post("/", authenticate, requireRole("author"), createPost)
+postsRouter.put("/:id", authenticate, requireRole("author"), updatePost)
+postsRouter.delete("/:id", authenticate, requireRole("author"), deletePost)
 
 module.exports = postsRouter
