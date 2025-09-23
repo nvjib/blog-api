629e37c5e6dae7d5cf0f14329d511ef8b6026a12
nvjib <nvjibiman@gmail.com>
2025-09-23 23:48:44 +0100

    refactor(auth): make emailSchema and passwordSchema resuable

--- validators/auth.validators.js
+++ validators/auth.validators.js
@@ -1,5 +1,19 @@
 const Joi = require("joi");
 
+const emailSchema = Joi.string().email().required().messages({
+    "string.base": "Email must be a string",
+    "string.empty": "Email is required",
+    "string.email": "Email must be a valid email",
+    "any.required": "Email is required"
+})
+
+const passwordSchema = Joi.string().min(6).required().messages({
+    "string.base": "Password must be a string",
+    "string.empty": "Password is required",
+    "string.min": "Password must be at least 6 characters",
+    "any.required": "Password is required"
+})
+
 const signUpSchema = Joi.object({
     name: Joi.string().min(3).max(20).required().messages({
         "string.base": "Name must be a string",
@@ -8,18 +22,8 @@ const signUpSchema = Joi.object({
         "string.max": "Name cannot exceed 20 characters",
         "any.required": "Name is required"
     }),
-    email: Joi.string().email().required().messages({
-        "string.base": "Email must be a string",
-        "string.empty": "Email is required",
-        "string.email": "Email must be a valid email",
-        "any.required": "Email is required"
-    }),
-    password: Joi.string().min(6).required().messages({
-        "string.base": "Password must be a string",
-        "string.empty": "Password is required",
-        "string.min": "Password must be at least 6 characters",
-        "any.required": "Password is required"
-    }),
+    email: emailSchema,
+    password: passwordSchema,
     role: Joi.string().valid("reader", "author").required().messages({
         "any.only": "Role must be either 'reader' or 'author'",
         "any.required": "Role is required"
@@ -27,18 +31,8 @@ const signUpSchema = Joi.object({
 })
 
 const loginSchema = Joi.object({
-    email: Joi.string().email().required().messages({
-        "string.base": "Email must be a string",
-        "string.empty": "Email is required",
-        "string.email": "Email must be a valid email",
-        "any.required": "Email is required"
-    }),
-    password: Joi.string().min(6).required().messages({
-        "string.base": "Password must be a string",
-        "string.empty": "Password is required",
-        "string.min": "Password must be at least 6 characters",
-        "any.required": "Password is required"
-    }),
+    email: emailSchema,
+    password: passwordSchema
 })
 
 module.exports = {
