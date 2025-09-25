a4d34805f46a887d80fcbc703b3524652d9995d2
nvjib <nvjibiman@gmail.com>
2025-09-25 23:40:20 +0100

    fix: test

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.use(express.json())
 app.use("/auth", auth)
 app.use("/posts", posts)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
