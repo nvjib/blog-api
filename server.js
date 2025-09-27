59c9382e91b2c2250c2b586a0f4a47685d67fcf2
nvjib <nvjibiman@gmail.com>
2025-09-27 23:58:56 +0100

    fix: port 3000

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.use(express.json())
 app.use("/auth", auth)
 app.use("/posts", posts)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
